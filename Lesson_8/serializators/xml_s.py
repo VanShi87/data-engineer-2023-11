aa82cfd56abf5ca43137fb367db986a266f343b8
Vanshi87 <shishov_ivan@list.ru>
2023-11-27 13:24:12 +0300

    add lesson8

--- Lesson_8/serializators/xml_s.py
+++ Lesson_8/serializators/xml_s.py
@@ -1,2 +1,27 @@
-def xml_serializator():
-    return 42
+import xml.etree.ElementTree as ET
+def xml_serializator(person_list):
+
+    def xml_element(person):
+        person_xml = ET.Element("Person")
+        name_xml = ET.SubElement(person_xml, "name")
+        name_xml.text = person.name
+        interests_xml = ET.SubElement(person_xml, "interests")
+        for interest in person.interests:
+            interest_xml = ET.SubElement(interests_xml, "interest")
+            interest_xml.text = interest
+        projectToHours_xml = ET.SubElement(person_xml, "projectToHours")
+        for project, hours in person.projectToHours.items():
+            project_xml = ET.SubElement(projectToHours_xml, "project")
+            project_xml.text = project
+            hours_xml = ET.SubElement(projectToHours_xml, "hours")
+            hours_xml.text = str(hours)
+        age_xml = ET.SubElement(person_xml, "age")
+        age_xml.text = str(person.age)
+        salary_xml = ET.SubElement(person_xml, "salary")
+        salary_xml.text = str(person.salary)
+        xml_string = ET.tostring(person_xml, encoding="utf-8")
+        return xml_string
+
+    with open('files/persons.xml', 'w') as file:
+        xml_strings = [xml_element(person) for person in person_list]
+        file.write('\n'.join(xml_strings))
